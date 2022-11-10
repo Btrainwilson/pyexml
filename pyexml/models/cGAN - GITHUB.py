dc8ef4a7a2f3c050fe4bb9a30bd837af8c6a10c1
michaelBezick <112839236+michaelBezick@users.noreply.github.com>
2022-11-10 08:59:03 -0500

    Add files via upload

---
+++ pyexml/models/cGAN - GITHUB.py
@@ -0,0 +1,56 @@
+import torch
+from torch import nn
+
+class Generator(nn.Module):
+    def __init__(self, hidden_dimensions = [256, 128, 64], kernel_size_list = [3, 4, 3, 4], stride_list = [2, 1, 2, 2], output_channels = 1, input_dim=1, im_chan=1, hidden_dim=64):
+      super(Generator, self).__init__()
+      self.input_dim = input_dim
+        
+        #Initial layer
+      self.h_layers = nn.ModuleList([nn.ConvTranspose2d(input_dim, hidden_dimensions[0], kernel_size=kernel_size_list[0], stride=stride_list[0])])
+      self.h_layers.extend([nn.BatchNorm2d(hidden_dimensions[0])])
+      self.h_layers.extend([nn.LeakyReLU(0.02, inplace=True)])
+        
+        #ReLU of hidden layers
+      for i in range(len(hidden_dimensions) - 1):
+          self.h_layers.extend([nn.ConvTranspose2d(hidden_dimensions[i], hidden_dimensions[i + 1], kernel_size=kernel_size_list[i+1], stride=stride_list[i+1])])
+          self.h_layers.extend([nn.BatchNorm2d(hidden_dimensions[i + 1])])
+          self.h_layers.extend([nn.LeakyReLU(0.02, inplace=True)])
+                                       
+        #final layer TanH function
+      self.h_layers.extend([nn.ConvTranspose2d(hidden_dimensions[-1], output_channels, kernel_size = kernel_size_list[-1], stride = stride_list[-1])])
+      self.h_layers.extend([nn.Tanh()])                           
+        
+    def forward(self, noise):
+      x = noise.view(len(noise), self.input_dim, 1, 1)
+      for i in range(len(self.h_layers) - 1):
+        x = self.h_layers[i](x)
+      
+      return self.h_layers[-1](x)
+
+class Discriminator(nn.Module):
+    def __init__(self, im_chan=1, hidden_dimensions = [64, 128], hidden_dim=64, in_channels = 1, output_channels = 1, kernel_size = 4, stride = 2):
+        super(Discriminator, self).__init__()
+        self.im_chan = im_chan
+        
+        #My code
+        self.h_layers = nn.ModuleList([nn.Conv2d(im_chan, hidden_dimensions[0], kernel_size, stride)])
+        self.h_layers.extend([nn.BatchNorm2d(hidden_dimensions[0])])
+        self.h_layers.extend([nn.LeakyReLU(0.02, inplace=True)])
+        
+        #ReLU of hidden layers
+        for i in range(len(hidden_dimensions) - 1):
+            self.h_layers.extend([nn.Conv2d(hidden_dimensions[i], hidden_dimensions[i + 1], kernel_size, stride)])
+            self.h_layers.extend([nn.BatchNorm2d(hidden_dimensions[i + 1])])
+            self.h_layers.extend([nn.LeakyReLU(0.02, inplace=True)])
+                                       
+        #final layer convolution
+        self.h_layers.extend([nn.Conv2d(hidden_dimensions[-1], im_chan, kernel_size, stride)])
+        
+        
+    def forward(self, image):
+      x = image
+      for i in range(len(self.h_layers) - 1):
+        x = self.h_layers[i](x)
+      disc_pred = self.h_layers[-1](x)
+      return disc_pred.view(len(disc_pred), -1)
